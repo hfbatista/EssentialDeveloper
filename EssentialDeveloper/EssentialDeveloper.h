71e9de0eb7ed0e7a7cb2144324420585cdacdef1
Henrique F. Batista <hfbatista@hotmail.com>
2024-02-07 20:27:36 +0000

    Initial Commit

---
+++ EssentialDeveloper/EssentialDeveloper.h
@@ -0,0 +1,18 @@
+//
+//  EssentialDeveloper.h
+//  EssentialDeveloper
+//
+//  Created by Henrique Batista on 07/02/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialDeveloper.
+FOUNDATION_EXPORT double EssentialDeveloperVersionNumber;
+
+//! Project version string for EssentialDeveloper.
+FOUNDATION_EXPORT const unsigned char EssentialDeveloperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialDeveloper/PublicHeader.h>
+
+
