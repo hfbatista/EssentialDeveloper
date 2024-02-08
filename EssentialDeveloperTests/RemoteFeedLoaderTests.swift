faf7dd3368bd7c719d5abdd7cea80492d55a2727
Henrique F. Batista <hfbatista@hotmail.com>
2024-02-08 22:49:31 +0000

    Create a global state example and move the logic of requested URL to HTTPClientSpy

--- EssentialDeveloperTests/RemoteFeedLoaderTests.swift
+++ EssentialDeveloperTests/RemoteFeedLoaderTests.swift
@@ -10,34 +10,41 @@ import XCTest
 
 class RemoteFeedLoader {
 	func load() {
-		HTTPCLient.shared.resquesteURL = URL(string: "htttp://a-url.com")
+		HTTPCLient.shared.get(from: URL(string: "htttp://a-url.com")!)
 	}
 }
 
 class HTTPCLient {
-	static let shared = HTTPCLient()
+	static var shared = HTTPCLient()
+	func get(from url: URL){}
+}
+
+class HTTPClientSpy: HTTPCLient {
+	override func get(from url: URL) {
+		requestedURL = url
+	}
 	
-	private init() {}
-	
-	var resquesteURL: URL?
+	var requestedURL: URL?
 }
 
 final class RemoteFeedLoaderTests: XCTestCase {
 
 	func test_init_doesNotResquestDataFromURL() {
-		let client = HTTPCLient.shared
+		let client = HTTPClientSpy()
+		HTTPCLient.shared = client
 		let _ = RemoteFeedLoader()
 		
-		XCTAssertNil(client.resquesteURL)
+		XCTAssertNil(client.requestedURL)
 	}
 	
 	func test_load_requestDataFromURL() {
-		let client = HTTPCLient.shared
+		let client = HTTPClientSpy()
+		HTTPCLient.shared = client
 		let sut  = RemoteFeedLoader()
 		
 		sut.load()
 		
-		XCTAssertNotNil(client.resquesteURL)
+		XCTAssertNotNil(client.requestedURL)
 	}
 
 }
