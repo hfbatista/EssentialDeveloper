6eddfd56cb569835ef64c7241c3216c87ea05c90
Henrique F. Batista <hfbatista@hotmail.com>
2024-02-08 22:40:33 +0000

    Create a Singleton reference for HTTPCLient

--- EssentialDeveloperTests/RemoteFeedLoaderTests.swift
+++ EssentialDeveloperTests/RemoteFeedLoaderTests.swift
@@ -9,20 +9,35 @@ import XCTest
 @testable import EssentialDeveloper
 
 class RemoteFeedLoader {
-	
+	func load() {
+		HTTPCLient.shared.resquesteURL = URL(string: "htttp://a-url.com")
+	}
 }
 
 class HTTPCLient {
+	static let shared = HTTPCLient()
+	
+	private init() {}
+	
 	var resquesteURL: URL?
 }
 
 final class RemoteFeedLoaderTests: XCTestCase {
 
 	func test_init_doesNotResquestDataFromURL() {
-		let client = HTTPCLient()
+		let client = HTTPCLient.shared
 		let _ = RemoteFeedLoader()
 		
 		XCTAssertNil(client.resquesteURL)
+	}
+	
+	func test_load_requestDataFromURL() {
+		let client = HTTPCLient.shared
+		let sut  = RemoteFeedLoader()
+		
+		sut.load()
+		
+		XCTAssertNotNil(client.resquesteURL)
 	}
 
 }
