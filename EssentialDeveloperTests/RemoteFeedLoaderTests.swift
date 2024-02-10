95778216f5f6953942d801fc4de354b33fbbabca
Henrique F. Batista <hfbatista@hotmail.com>
2024-02-10 11:31:27 +0000

    Gua=rantee we do note deliver a result or invoke completion afte RemoteFeedLoader has been dealocated

--- EssentialDeveloperTests/RemoteFeedLoaderTests.swift
+++ EssentialDeveloperTests/RemoteFeedLoaderTests.swift
@@ -93,6 +93,20 @@ final class RemoteFeedLoaderTests: XCTestCase {
 		}
 	}
 	
+	func test_load_doesNotDeliversResultAfterSUTHasBeenDealocated() {
+		let url = URL(string: "https://any-url.com")!
+		let client = HTTPClientSpy()
+		var sut: RemoteFeedLoader? = RemoteFeedLoader(url: url, client: client)
+		
+		var capturedResults = [RemoteFeedLoader.Result]()
+		sut?.load() { capturedResults.append($0) }
+		
+		sut = nil
+		client.complete(withStatusCode: 200, data: makeJSONItems([]))
+		
+		XCTAssertTrue(capturedResults.isEmpty)
+	}
+	
 	//MARK: - Helpers
 	
 	private func makeSUT(url: URL = URL(string: "http://a-url.com")!, file: StaticString = #filePath, line: UInt = #line) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
