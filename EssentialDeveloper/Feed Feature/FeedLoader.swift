f46afabdf4b17badc2c8b7527874585e7ea22f86
Henrique F. Batista <hfbatista@hotmail.com>
2024-02-10 12:23:10 +0000

    Make RemoteFeedLoaderProtocol public

--- EssentialDeveloper/Feed Feature/FeedLoader.swift
+++ EssentialDeveloper/Feed Feature/FeedLoader.swift
@@ -12,6 +12,6 @@ public enum LoadFeedResult {
 	case failure(Error)
 }
 
-protocol FeedLoaderProtocol {
+public protocol FeedLoaderProtocol {
 	func load (completion: @escaping (LoadFeedResult) -> Void)
 }
