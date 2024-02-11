cbc624cf2f2d3ad3437a6c6e0574163b3cafbae7
Henrique F. Batista <hfbatista@hotmail.com>
2024-02-11 01:24:43 +0000

    Guarantees correct URL and HTTP method on get request

--- EssentialDeveloperTests/Feed API/HTTPClientTests.swift
+++ EssentialDeveloperTests/Feed API/HTTPClientTests.swift
@@ -16,7 +16,6 @@ private class URLSessionHTTPClient {
 	}
 	
 	func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
-		let url = URL(string: "http://a-wrong-url.com")!
 		session.dataTask(with: url) { _,_,error in
 			if let error {
 				completion(.failure(error))
@@ -26,6 +25,24 @@ private class URLSessionHTTPClient {
 }
 
 final class HTTPClientTests: XCTestCase {
+	
+	func test_getFromURL_performGETRequestWithURL() {
+		URLProtocolStub.startInterceptingRequests()
+		let url = URL(string: "http://a-given-url.com")!
+		let exp = expectation(description: "Wait for request")
+		
+		URLProtocolStub.observeRequests { request in
+			XCTAssertEqual(request.url, url)
+			XCTAssertEqual(request.httpMethod, "GET")
+			exp.fulfill()
+		}
+		
+		URLSessionHTTPClient().get(from: url) { _ in }
+		
+		wait(for: [exp], timeout: 1.0)
+		URLProtocolStub.stopInterceptingRequests()
+	}
+	
 	func test_getFromURL_failsOnResquestError() {
 		URLProtocolStub.startInterceptingRequests()
 		let url = URL(string: "http://a-url.com")!
@@ -55,6 +72,7 @@ final class HTTPClientTests: XCTestCase {
 	
 	private class URLProtocolStub: URLProtocol {
 		private static var stub: StubModel?
+		private static var requestObserver: ((URLRequest) -> Void)?
 		
 		private struct StubModel {
 			let data: Data?
@@ -73,9 +91,15 @@ final class HTTPClientTests: XCTestCase {
 		static func stopInterceptingRequests() {
 			URLProtocol.unregisterClass(URLProtocolStub.self)
 			stub = nil
+			requestObserver = nil
+		}
+		
+		static func observeRequests(observer: @escaping (URLRequest) -> Void) {
+			requestObserver = observer
 		}
 		
 		override class func canInit(with request: URLRequest) -> Bool {
+			requestObserver?(request)
 			return true
 		}
 		
