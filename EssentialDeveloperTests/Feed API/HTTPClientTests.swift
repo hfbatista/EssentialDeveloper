08e137d6a3f77a234a54671f7d1732ac22d5955d
Henrique F. Batista <hfbatista@hotmail.com>
2024-02-14 00:00:52 +0000

    move URLSessionHTTPClient to its own file

--- EssentialDeveloperTests/Feed API/HTTPClientTests.swift
+++ EssentialDeveloperTests/Feed API/HTTPClientTests.swift
@@ -8,28 +8,6 @@
 import XCTest
 import EssentialDeveloper
 
-private class URLSessionHTTPClient: HTTPCLientProtocol {
-	private let session: URLSession
-	
-	init(session: URLSession = .shared) {
-		self.session = session
-	}
-	
-	struct UnexpectedErroWithNoValiues: Error {}
-	
-	func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
-		session.dataTask(with: url) { data, response, error in
-			if let error {
-				completion(.failure(error))
-			} else if let data = data, let response = response as? HTTPURLResponse {
-				completion(.success(data, response))
-			} else {
-				completion(.failure(UnexpectedErroWithNoValiues()))
-			}
-		}.resume()
-	}
-}
-
 final class HTTPClientTests: XCTestCase {
 	
 	override class func setUp() {
