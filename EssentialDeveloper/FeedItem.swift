93666980436e9bbd4d5851be336c33c33e31b8fe
Henrique F. Batista <hfbatista@hotmail.com>
2024-02-18 21:20:30 +0000

    reverting pbproject mess

---
+++ EssentialDeveloper/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialDeveloper
+//
+//  Created by Henrique Batista on 08/02/24.
+//
+
+import Foundation
+
+struct FeedItemModel {
+	let id: UUID
+	let description: String?
+	let location: String?
+	let imageURL: URL
+}
